bc84980d4f016dfec40eec01aa005786b154c64d
leandrojamir <leandrojamir@hotmail.com>
2023-02-20 09:58:16 -0300

    docs: faltou os warnings do serverTS

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,6 @@ import connectToDatabase from './Models/Connection';
 const PORT = process.env.PORT || 3001;
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+    app.listen(PORT, () => (`Running server on port: ${PORT}`));
   })
-  .catch((error) => {
-    console.log('Connection with database generated an error:\r\n');
-    console.error(error);
-    console.log('\r\nServer initialization cancelled');
-    process.exit(0);
-  });
+  .catch(() => process.exit(0));
